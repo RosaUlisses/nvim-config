0010d956c87683ffc219a85212b2312162bc570f
RosaUlisses <ulissao@proton.me>
2024-06-04 12:32:23 -0300

    remove comments from lazy.lua file

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -1,8 +1,6 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  -- bootstrap lazy.nvim
-  -- stylua: ignore
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
@@ -21,24 +19,15 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
   },
   defaults = {
-    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
     lazy = false,
-    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-    -- have outdated releases, which may break your Neovim install.
-    version = false, -- always use the latest git commit
-    -- version = "*", -- try installing the latest stable version for plugins that support semver
+    version = false,
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
+  checker = { enabled = true },
   performance = {
     rtp = {
-      -- disable some rtp plugins
       disabled_plugins = {
         "gzip",
-        -- "matchit",
-        -- "matchparen",
-        -- "netrwPlugin",
         "tarPlugin",
         "tohtml",
         "tutor",
@@ -48,7 +37,6 @@ require("lazy").setup({
   },
 })
 
--- changing the default shell, here I set to nushell
 vim.o.shell = "/home/ullas/.cargo/bin/nu"
 
 require("hop").setup({})
